561bb12e672f10238a0570ae30ffe9c7f700cb5f
mcanbildiren <78155451+mcanbildiren@users.noreply.github.com>
2022-05-20 10:13:19 +0300

    viewmodels

---
+++ TechnicalServiceProject/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalServiceProject.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Adresi")]
+        [Required(ErrorMessage = "Email adresi gereklidir.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre gereklidir.")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla")]
+        public bool RememberMe { get; set; }
+    }
+}
--- TechnicalServiceProject/ViewModels/RegisterViewModel.cs
+++ TechnicalServiceProject/ViewModels/RegisterViewModel.cs
@@ -5,27 +5,27 @@ namespace AdminTemplate.ViewModels;
 public class RegisterViewModel
 {
     [Display(Name = "Ad")]
-    [Required(ErrorMessage = "Ad alanı gereklidir.")]
+    [Required(ErrorMessage = "Ad gereklidir.")]
     [StringLength(50)]
     public string Name { get; set; }
 
     [Display(Name = "Soyad")]
-    [Required(ErrorMessage = "Soyad alanı gereklidir.")]
+    [Required(ErrorMessage = "Soyad gereklidir.")]
     [StringLength(50)]
     public string Surname { get; set; }
 
     [EmailAddress]
-    [Required(ErrorMessage = "E-Posta alanı gereklidir.")]
+    [Required(ErrorMessage = "E-Posta gereklidir.")]
     public string Email { get; set; }
 
     [Display(Name = "Şifre")]
-    [Required(ErrorMessage = "Şifre alanı gereklidir.")]
+    [Required(ErrorMessage = "Şifre gereklidir.")]
     [StringLength(100, MinimumLength = 6, ErrorMessage = "Şifreniz minimum 6 karakterli olmalıdır!")]
     [DataType(DataType.Password)]
     public string Password { get; set; }
 
-    [Display(Name = "Şifre Tekrar")]
-    [Required(ErrorMessage = "Şifre tekrar alanı gereklidir.")]
+    [Display(Name = "Şifre Tekrarı")]
+    [Required(ErrorMessage = "Şifre tekrarı gereklidir.")]
     [DataType(DataType.Password)]
     [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor!")]
     public string ConfirmPassword { get; set; }
