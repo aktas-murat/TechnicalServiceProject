1130af5137c79e8ed39264c61982270e3a3785ff
Murat AKTAŞ <Murat.Aktaas@hotmail.com>
2022-05-31 15:46:39 +0300

    roles update

--- TechnicalService.Core/Models/Role/Roles.cs
+++ TechnicalService.Core/Models/Role/Roles.cs
@@ -5,10 +5,12 @@
         public static readonly string Admin = "Admin";
         public static readonly string User = "User";
         public static readonly string Passive = "Passsive";
+        public static readonly string Operator = "Operator";
+        public static readonly string Technician = "Technician";
 
         public static List<string> RoleList = new List<string>()
         {
-            Admin, User, Passive
+            Admin, User, Passive, Operator, Technician
         };
     }
 }
