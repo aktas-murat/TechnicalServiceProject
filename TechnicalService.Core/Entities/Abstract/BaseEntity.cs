e7a509d40b74c6c7eaecb1ec055af90e9efbe224
Murat AKTAŞ <Murat.Aktaas@hotmail.com>
2022-06-07 17:19:58 +0300

    Base Entity and ServiceDemand  added ,ServiceForm Update.

---
+++ TechnicalService.Core/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalService.Core.Entities.Abstract
+{
+    public abstract class BaseEntity<T> where T : IEquatable<T>
+
+    {
+        public T Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ TechnicalService.Core/Entities/ServiceDemand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalService.Core.Entities.Abstract;
+
+namespace TechnicalService.Core.Entities; 
+
+public class ServiceDemand : BaseEntity <int>
+{
+    public string Name { get; set; }
+    public string SurName  { get; set; }
+    public string Email { get; set; }
+    public string BuildingNo { get; set; }
+    public string FloorNo { get; set; }
+    public string DoorNo { get; set; }
+    public string Phone { get; set; }
+    public string Message { get; set; }
+    public string Adress { get; set; }
+    public string UserId { get; set; }
+
+}
