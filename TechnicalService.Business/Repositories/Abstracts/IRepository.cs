51491bd9e436675244b862fe87347caced6742ef
Serkan Ozsoz <serkanozsoz@hotmail.com>
2022-06-15 11:55:59 +0300

    Repository Classes Created

---
+++ TechnicalService.Business/Repositories/Abstracts/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalService.Business.Repositories.Abstracts
+{
+    public interface IRepository<TEntity, TKey>
+        where TKey : IEquatable<TKey>
+        where TEntity : class
+    {
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate = null);
+        //Task<IQueryable<TEntity>> GetAsnyc(Expression<Func<TEntity, bool>> predicate = null);
+        TEntity GetById(TKey id);
+        int Insert(TEntity entity, bool isSaveLater = false);
+        int Update(TEntity entity, bool isSaveLater = false);
+        int Delete(TEntity entity, bool isSaveLater = false);
+        int Save();
+    }
+}
